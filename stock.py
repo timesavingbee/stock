a378079c63f4bde5d7f564b460adcad1f08ec7f8
timesavingbee <130218840+timesavingbee@users.noreply.github.com>
2023-04-15 16:06:12 +0900

    Update stock.py

--- stock.py
+++ stock.py
@@ -1,5 +1,8 @@
-import FinanceDataReader as fdr #재무 정보 다운로드
+#import FinanceDataReader as fdr #재무 정보 다운로드
 import pandas as pd
-code_a='086520' #에코프로
-df_a=fdr.DataReader(code_a)
-df_a.plot()
+import streamlit as st
+# code_a='086520' #에코프로
+# df_a=fdr.DataReader(code_a)
+# df_a.plot()
+
+print('test')
