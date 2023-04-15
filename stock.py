93245ad942d045114729eef5bfb3d25ea17e6e56
timesavingbee <130218840+timesavingbee@users.noreply.github.com>
2023-04-15 16:32:12 +0900

    Update stock.py

--- stock.py
+++ stock.py
@@ -25,10 +25,10 @@ ax.hist(arr, bins=20)
 
 fig  # 👈 Draw a Matplotlib chart
 
-import FinanceDataReader as fdr #재무 정보 다운로드
-# import pandas as pd
-code_a='086520' #에코프로
-df_a=fdr.DataReader(code_a)
-# df_a.plot()
+# import FinanceDataReader as fdr #재무 정보 다운로드
+# # import pandas as pd
+# code_a='086520' #에코프로
+# df_a=fdr.DataReader(code_a)
+# # df_a.plot()
 
-df_a
+# df_a
